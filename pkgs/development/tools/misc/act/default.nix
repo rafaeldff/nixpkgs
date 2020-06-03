75130827e98c0c45f0eef19ab50d91fadf597c48
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2020-06-03 16:38:08 -0300

    Update nektos/act

--- pkgs/development/tools/misc/act/default.nix
+++ pkgs/development/tools/misc/act/default.nix
@@ -2,22 +2,21 @@
 
 buildGoModule rec {
   pname = "act";
-  version = "0.1.3";
+  version = "0.2.9";
 
   src = fetchFromGitHub {
     owner = "nektos";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1y1bvk93dzsxwjakmgpb5qyy3lqng7cdabi64b555c1z6b42mf58";
+    sha256 = "17w7pqpn9pkzc85lrsmyhxy6rip47dxw1hkz4ml3y5n68nysfpb9";
   };
 
-  modSha256 = "00d0wjnr5y3bl95lma8sdwvqqs7fd0k43azawp1kb29kqnrlismg";
+  modSha256 = "1bdczh9hzkrs6xb2m2aag674m1dc572pd5iplvr3rkf2a45dmqac";
 
   meta = with lib; {
     description = "Run your GitHub Actions locally";
     homepage = "https://circleci.com/";
     license = licenses.mit;
     maintainers = with maintainers; [ filalex77 ];
-    broken = true;
   };
 }
